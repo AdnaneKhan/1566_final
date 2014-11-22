5a9407162f7ca7193cf6eff7a9955fa8a423bb6d
Adnan <adnankhan@gmail.com>
2014-11-21 21:35:43 -0500

    rmeoved temp

---
+++ Header1.h
@@ -0,0 +1,20 @@
+#ifndef SPACESHIP_H
+#define SPACESHIP_H
+
+class Spaceship {
+
+public:
+
+	// Rotation matrix representing position of ship
+	private GLfloat rotation[16];
+
+
+
+
+private:
+
+	
+};
+
+
+#endif // END SPACESHIP_H GUARD
