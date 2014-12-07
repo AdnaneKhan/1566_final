f818f387143775ca1fe89c925f674f1396031a09
thelastviolin <kdi8@pitt.edu>
2014-12-07 17:28:32 -0500

    Renamed variables and changed description

--- src/Ray.hpp
+++ src/Ray.hpp
@@ -5,13 +5,14 @@
 Keeps track of the direction of a ray, along with the unit vector
 calculated during intialization. The unit vector has been added such
 that we have the normalization of the vector and the initial direction
-is still saved, probably for debug.
+is still saved, probably for debug. Dir represents the unit vector and
+original are the arguments passed.
 
 x, y, z are 0, 1, 2 respectively in the arrays declared publicly.
 */
 class Ray {
 public:
+	float Original[3];
 	float Dir[3];
-	float UnitVector[3];
 	Ray(float x, float y, float z);
 };
