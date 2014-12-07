bae601fec1eb45841841dfed96015be7eda25014
thelastviolin <kdi8@pitt.edu>
2014-12-07 17:29:43 -0500

    Renamed variables and changed description

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -1,11 +1,11 @@
 #include "Ray.hpp"
 
 Ray::Ray(float x, float y, float z) {
-	Dir[0] = x;
-	Dir[1] = y;
-	Dir[2] = z;
+	Original[0] = x;
+	Original[1] = y;
+	Original[2] = z;
 
-	UnitVector[0] = x / sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-	UnitVector[1] = y / sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-	UnitVector[2] = z / sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+	Dir[0] = x / sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+	Dir[1] = y / sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+	Dir[2] = z / sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
 }
