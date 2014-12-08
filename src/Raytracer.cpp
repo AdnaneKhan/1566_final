b32706a35ce016df16565648f58a2298181fb8a7
thelastviolin <kdi8@pitt.edu>
2014-12-07 19:09:05 -0500

    Finished calculating of cameraRay

--- src/Raytracer.cpp
+++ src/Raytracer.cpp
@@ -1,1 +1,19 @@
 #include "Raytracer.hpp"
+
+#define PI 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328
+
+void Raytrace(float eyeX, float eyeY, float eyeZ, float fieldOfView, float windowW, float windowH) {
+	float xBlock, yBlock, zBlock;
+	float midpointW, midpointH;
+
+	midpointW = windowW / 2;
+	midpointH = windowH / 2;
+
+	for (int w = 0; w < windowW; w++) {
+		for (int h = 0; h < windowH; h++) {
+			Ray cameraRay = Ray(w-midpointW, h-midpointH, midpointW/tan(fieldOfView*.5*(PI/180)));
+			cameraRay.SetOrigin(eyeX, eyeY, eyeZ);
+
+		}
+	}
+}
