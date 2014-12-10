49eaddfd26c72484b0a6f0c0bfae4e00231c7699
thelastviolin <kdi8@pitt.edu>
2014-12-09 21:38:22 -0500

    Same as before

--- src/Space_Object.hpp
+++ src/Space_Object.hpp
@@ -33,7 +33,7 @@ class Space_Object : public Drawable {
 	int planet_radius;
 	GLfloat world_pos[3];
 	std::list<Space_Object *> satellites;
-	std::list<Intersection *> toBeShaded;
+	std::list<Vertex *> vertices;
     int getObjectType();
 
     /**
