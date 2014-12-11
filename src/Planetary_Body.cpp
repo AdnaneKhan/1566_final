2b9b6cf76eba45fb0d4a7a1df55b925f304b7256
Adnan <adnankhan@gmail.com>
2014-12-11 10:15:09 -0500

    tweaks

--- src/Planetary_Body.cpp
+++ src/Planetary_Body.cpp
@@ -67,6 +67,7 @@ Planetary_Body::Planetary_Body(float rotationRate, int radius, textureType tType
 	random_rot_axis(this->rotationAxis);
 	this->rotation = 0;
 	random_rot_axis(this->orbit_plane.planeNormal);
+
 	this->draw_type = STAR;
 	this->planet_tex = &texture_pool[tType];
 	createPlanet(&this->actual_planet, this->draw_type, this->planet_radius, .1);
